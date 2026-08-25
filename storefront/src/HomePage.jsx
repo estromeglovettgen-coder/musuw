2c25edc9cc9c774e2d8478c8ca70e035afba781f
estromeglovettgen-coder <estromeglovettgen@gmail.com>
2026-08-25 15:05:37 -0700

    Reorder homepage around the visitor decision journey

--- storefront/src/HomePage.jsx
+++ storefront/src/HomePage.jsx
@@ -1,30 +1,35 @@
 import { HeroScene } from "./components/HeroScene";
 import {
   BenefitsSection,
-  BlogPreviewSection,
-  ComparisonSection,
   FAQSection,
   FeaturesSection,
   FinalCTA,
+  IncludedInEveryPlanSection,
+  JourneyStrip,
   PricingSection,
-  WorkflowSection
+  UseCasesSection,
+  WorkflowSection,
 } from "./components/HomeSections";
 import { SiteFooter, SiteHeader } from "./components/SiteChrome";
+import { getHomeJourney } from "./data/homeJourney";
 
-export function HomePage({ copy }) {
+export function HomePage({ copy, locale }) {
+  const content = getHomeJourney(locale);
+
   return (
     <>
       <SiteHeader copy={copy} />
-      <main>
-        <HeroScene copy={copy} />
-        <FeaturesSection copy={copy} />
-        <WorkflowSection copy={copy} />
-        <BenefitsSection copy={copy} />
-        <PricingSection copy={copy} />
-        <ComparisonSection copy={copy} />
-        <BlogPreviewSection copy={copy} />
-        <FAQSection copy={copy} />
-        <FinalCTA copy={copy} />
+      <main className="home-journey">
+        <HeroScene content={content.hero} />
+        <JourneyStrip content={content.journey} />
+        <FeaturesSection content={content.features} />
+        <WorkflowSection content={content.workflow} />
+        <UseCasesSection content={content.useCases} />
+        <BenefitsSection content={content.trust} />
+        <PricingSection content={content.pricing} locale={locale} />
+        <IncludedInEveryPlanSection content={content.included} />
+        <FAQSection content={content.faq} />
+        <FinalCTA content={content.finalCta} />
       </main>
       <SiteFooter copy={copy} />
     </>
