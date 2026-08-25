e78f3a64926dbcd76044086f8d95415450c7f1b4
estromeglovettgen-coder <estromeglovettgen@gmail.com>
2026-08-25 15:09:18 -0700

    Load homepage journey styles after base storefront CSS

--- storefront/src/main.jsx
+++ storefront/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./legal.css";
+import "./home-journey.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
